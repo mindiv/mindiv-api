2bae5645d176076b50d65afd6b848933a9ba9d39
jerry <jerrynwosu007@gmail.com>
2023-01-04 13:32:52 +0100

    update: users module config

---
+++ src/modules/users/dto/patch.user.dto.ts
@@ -0,0 +1,3 @@
+import { PutUserDto } from './put.user.dto';
+
+export interface PatchUseDto extends Partial<PutUserDto> {}
---
+++ src/modules/users/dto/put.user.dto.ts
@@ -0,0 +1,8 @@
+export interface PutUserDto {
+  id: string;
+  email: string;
+  password: string;
+  fisrtName: string;
+  lastName: string;
+  permissionLevel: number;
+}
